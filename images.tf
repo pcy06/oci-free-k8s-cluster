dbbfe33bfac2ab913c118095b99525b218229c50
PARK CHAN YOUNG <pcy06@kakao.com>
2024-04-21 22:26:29 +0900

    feat: add ubuntu 22.04 image

--- images.tf
+++ images.tf
@@ -7,3 +7,13 @@ data "oci_core_images" "ubuntu-20-04-arm" {
         regex = true
     }
 }
+
+data "oci_core_images" "ubuntu-22-04-arm" {
+    compartment_id = var.compartment_ocid
+    operating_system = "Canonical Ubuntu"
+    filter {
+        name   = "display_name"
+        values = ["^Canonical-Ubuntu-22.04-aarch64-([\\.0-9-]+)$"]
+        regex = true
+    }
+}
